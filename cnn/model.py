fa222f6f4207134331600e96370f40b169eecbf1
dassh <testzx@foxmail.com>
2018-11-12 10:31:11 +0800

    support digit 0~50

--- cnn/model.py
+++ cnn/model.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
+
+
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
@@ -16,13 +18,50 @@ def max_pool_2x2(x):
 
 
 def model_number(x, keep_prob):
-    return convolutional(x, keep_prob, 10)
-    
+    return convolutional(x, keep_prob, 51)
+     
 def model_right_wrong(x, keep_prob):
-    return convolutional(x, keep_prob, 2)
-    
+    return convolutional_right_wrong(x, keep_prob, 2)
     
 def convolutional(x, keep_prob, label_length):
+
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+    W_conv1 = weight_variable([3, 3, 1, 256])
+    b_conv1 = bias_variable([256])
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_bn1   = tf.contrib.layers.batch_norm(h_conv1)
+
+    W_conv2 = weight_variable([3, 3, 256, 128])
+    b_conv2 = bias_variable([128])
+    h_conv2 = tf.nn.relu(conv2d(h_bn1, W_conv2) + b_conv2)
+    h_pool2 = max_pool_2x2(h_conv2)  # 14 * 14
+    
+    W_conv3 = weight_variable([3, 3, 128, 128])
+    b_conv3 = bias_variable([128])
+    h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
+    h_bn3   = tf.contrib.layers.batch_norm(h_conv3)
+
+    W_conv4 = weight_variable([3, 3, 128, 64])
+    b_conv4 = bias_variable([64])
+    h_conv4 = tf.nn.relu(conv2d(h_bn3, W_conv4) + b_conv4)
+    h_pool4 = max_pool_2x2(h_conv4)  # 7 * 7
+    
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+
+    h_pool4_flat = tf.reshape(h_pool4, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool4_flat, W_fc1) + b_fc1)
+
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+    W_fc2 = weight_variable([1024, label_length])
+    b_fc2 = bias_variable([label_length])
+
+    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+    return y_conv, [W_conv1, b_conv1, W_conv2, b_conv2, W_conv3, b_conv3, W_conv4, b_conv4, W_fc1, b_fc1, W_fc2, b_fc2]
+    
+def convolutional_right_wrong(x, keep_prob, label_length):
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
 
